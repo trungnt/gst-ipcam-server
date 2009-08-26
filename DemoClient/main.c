dcbedf6c50f72dfe1ec17987b38126fd2175bac1
Dam Quang Tuan <damquan.tuan@nomovok.com>
2009-08-26 09:56:16 -0400

    created a demo client

---
+++ DemoClient/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: dqtuan
+ *
+ * Created on August 26, 2009, 9:54 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
