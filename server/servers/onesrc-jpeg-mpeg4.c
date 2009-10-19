ec2eab23ddc7e35866f40a5221e18c7ed42a9069
Nguyen Thanh Trung <nguyenthanh.trung@nomovok.com>
2009-10-19 14:42:22 +0700

    server code review:expand tab to 2 spaces

--- server/servers/onesrc-jpeg-mpeg4.c
+++ server/servers/onesrc-jpeg-mpeg4.c
@@ -28,102 +28,102 @@
 
 static gboolean
 timeout(GstRTSPServer *server, gboolean ignored) {
-	GstRTSPSessionPool *pool;
+  GstRTSPSessionPool *pool;
 
-	pool = gst_rtsp_server_get_session_pool(server);
-	gst_rtsp_session_pool_cleanup(pool);
-	g_object_unref(pool);
+  pool = gst_rtsp_server_get_session_pool(server);
+  gst_rtsp_session_pool_cleanup(pool);
+  g_object_unref(pool);
 
-	return TRUE;
+  return TRUE;
 }
 
 int
 main(int argc, char *argv[]) {
-	GMainLoop *loop;
-	GstRTSPServer *server;
-	GstRTSPMediaMapping *mapping;
-	GstRTSPMediaFactory *factory_mpeg4, *factory_jpg;
-	GstRTSPServerConfiguration * server_config;
-	gchar * audio_stream_type = NULL;
+  GMainLoop *loop;
+  GstRTSPServer *server;
+  GstRTSPMediaMapping *mapping;
+  GstRTSPMediaFactory *factory_mpeg4, *factory_jpg;
+  GstRTSPServerConfiguration * server_config;
+  gchar * audio_stream_type = NULL;
 
-	gst_init(&argc, &argv);
+  gst_init(&argc, &argv);
 
-	loop = g_main_loop_new(NULL, FALSE);
+  loop = g_main_loop_new(NULL, FALSE);
 
-	/* create a server instance */
-	server = gst_rtsp_server_new();
+  /* create a server instance */
+  server = gst_rtsp_server_new();
 
-	/* get the mapping for this server, every server has a default mapper object
-	 * that be used to map uri mount points to media factories */
-	mapping = gst_rtsp_server_get_media_mapping(server);
+  /* get the mapping for this server, every server has a default mapper object
+   * that be used to map uri mount points to media factories */
+  mapping = gst_rtsp_server_get_media_mapping(server);
 
-	/* make a media factory for a mpeg 4 video stream and audio (aac, g711 or g726) stream. The default media factory can use
-	 * gst-launch syntax to create pipelines.
-	 * any launch line works as long as it contains elements named pay%d. Each
-	 * element with pay%d names will be a stream */
-	factory_mpeg4 = gst_rtsp_media_factory_new();
-	factory_mpeg4->two_streams = TRUE;
-	/* set webcam source and port to listen for factory */
-	gst_rtsp_factory_set_device_source(factory_mpeg4, "v4l2src", "/dev/video0", 3000);
+  /* make a media factory for a mpeg 4 video stream and audio (aac, g711 or g726) stream. The default media factory can use
+   * gst-launch syntax to create pipelines.
+   * any launch line works as long as it contains elements named pay%d. Each
+   * element with pay%d names will be a stream */
+  factory_mpeg4 = gst_rtsp_media_factory_new();
+  factory_mpeg4->two_streams = TRUE;
+  /* set webcam source and port to listen for factory */
+  gst_rtsp_factory_set_device_source(factory_mpeg4, "v4l2src", "/dev/video0", 3000);
 
-	/* prepare server configration for mpeg4 stream */
-	server_config = gst_rtsp_server_configuration_load(DEFAULT_PROFILE_FILE_VIDEO_MPEG4);
-	/* check and set audio profile for mpeg4 server configuration */
-	if (argc > 1) {
-		if (g_strrstr(argv[1], "aac")) {
-			audio_stream_type = "audio AAC";
-		} else if (g_strrstr(argv[1], "g726")) {
-			audio_stream_type = "audio G726";
-		} else if (g_strrstr(argv[1], "g711")) {
-			audio_stream_type = "audio G711";
-		}
-		if (audio_stream_type != NULL) {
-			gst_rtsp_server_configuration_set_default_audio_pipeline(server_config, audio_stream_type);
-		}
-	}
+  /* prepare server configration for mpeg4 stream */
+  server_config = gst_rtsp_server_configuration_load(DEFAULT_PROFILE_FILE_VIDEO_MPEG4);
+  /* check and set audio profile for mpeg4 server configuration */
+  if (argc > 1) {
+    if (g_strrstr(argv[1], "aac")) {
+      audio_stream_type = "audio AAC";
+    } else if (g_strrstr(argv[1], "g726")) {
+      audio_stream_type = "audio G726";
+    } else if (g_strrstr(argv[1], "g711")) {
+      audio_stream_type = "audio G711";
+    }
+    if (audio_stream_type != NULL) {
+      gst_rtsp_server_configuration_set_default_audio_pipeline(server_config, audio_stream_type);
+    }
+  }
 
-	/* map mpeg4 server configuration to mpeg4 media factory */
-	gst_rtsp_media_factory_set_server_configuration(factory_mpeg4, server_config);
+  /* map mpeg4 server configuration to mpeg4 media factory */
+  gst_rtsp_media_factory_set_server_configuration(factory_mpeg4, server_config);
 
-	/* share the pipeline with multiple clients */
-	gst_rtsp_media_factory_set_shared(factory_mpeg4, TRUE);
+  /* share the pipeline with multiple clients */
+  gst_rtsp_media_factory_set_shared(factory_mpeg4, TRUE);
 
-	/* attach the test factory to the /mp4 url */
-	gst_rtsp_media_mapping_add_factory(mapping, "/mp4", factory_mpeg4);
+  /* attach the test factory to the /mp4 url */
+  gst_rtsp_media_mapping_add_factory(mapping, "/mp4", factory_mpeg4);
 
-	/* make a media factory for jpeg stream. The default media factory can use
-	 * gst-launch syntax to create pipelines.
-	 * any launch line works as long as it contains elements named pay%d. Each
-	 * element with pay%d names will be a stream */
-	factory_jpg = gst_rtsp_media_factory_new();
-	factory_jpg->two_streams = TRUE;
-	/* set webcam source and port to listen for server */
-	/* gst_rtsp_factory_set_device_source (factory_jpg, "v4l2src", "/dev/video0", 3000); */
-	factory_jpg->v4l2src_pipeline = factory_mpeg4->v4l2src_pipeline;
-	factory_jpg->v4l2src_port = factory_mpeg4->v4l2src_port + 1;
-	factory_jpg->multiudpsink = factory_mpeg4->multiudpsink;
+  /* make a media factory for jpeg stream. The default media factory can use
+   * gst-launch syntax to create pipelines.
+   * any launch line works as long as it contains elements named pay%d. Each
+   * element with pay%d names will be a stream */
+  factory_jpg = gst_rtsp_media_factory_new();
+  factory_jpg->two_streams = TRUE;
+  /* set webcam source and port to listen for server */
+  /* gst_rtsp_factory_set_device_source (factory_jpg, "v4l2src", "/dev/video0", 3000); */
+  factory_jpg->v4l2src_pipeline = factory_mpeg4->v4l2src_pipeline;
+  factory_jpg->v4l2src_port = factory_mpeg4->v4l2src_port + 1;
+  factory_jpg->multiudpsink = factory_mpeg4->multiudpsink;
 
-	/* prepare server configuration for jpeg stream */
-	server_config = gst_rtsp_server_configuration_load(DEFAULT_PROFILE_FILE_JPEG);
-	/* map server configuration to media factory */
-	gst_rtsp_media_factory_set_server_configuration(factory_jpg, server_config);
+  /* prepare server configuration for jpeg stream */
+  server_config = gst_rtsp_server_configuration_load(DEFAULT_PROFILE_FILE_JPEG);
+  /* map server configuration to media factory */
+  gst_rtsp_media_factory_set_server_configuration(factory_jpg, server_config);
 
-	/* share the pipeline with multiple clients */
-	gst_rtsp_media_factory_set_shared(factory_jpg, TRUE);
+  /* share the pipeline with multiple clients */
+  gst_rtsp_media_factory_set_shared(factory_jpg, TRUE);
 
-	/* attach the test factory to the /jpg url */
-	gst_rtsp_media_mapping_add_factory(mapping, "/jpg", factory_jpg);
+  /* attach the test factory to the /jpg url */
+  gst_rtsp_media_mapping_add_factory(mapping, "/jpg", factory_jpg);
 
-	/* don't need the ref to the mapper anymore */
-	g_object_unref(mapping);
+  /* don't need the ref to the mapper anymore */
+  g_object_unref(mapping);
 
-	/* attach the server to the default maincontext */
-	gst_rtsp_server_attach(server, NULL);
+  /* attach the server to the default maincontext */
+  gst_rtsp_server_attach(server, NULL);
 
-	g_timeout_add_seconds(2, (GSourceFunc) timeout, server);
+  g_timeout_add_seconds(2, (GSourceFunc) timeout, server);
 
-	/* start serving */
-	g_main_loop_run(loop);
+  /* start serving */
+  g_main_loop_run(loop);
 
-	return 0;
+  return 0;
 }
--- server/servers/twosrc-jpeg-h264.c
+++ server/servers/twosrc-jpeg-h264.c
@@ -28,100 +28,100 @@
 
 static gboolean
 timeout(GstRTSPServer *server, gboolean ignored) {
-	GstRTSPSessionPool *pool;
+  GstRTSPSessionPool *pool;
 
-	pool = gst_rtsp_server_get_session_pool(server);
-	gst_rtsp_session_pool_cleanup(pool);
-	g_message("Server timeout");
-	g_object_unref(pool);
+  pool = gst_rtsp_server_get_session_pool(server);
+  gst_rtsp_session_pool_cleanup(pool);
+  g_message("Server timeout");
+  g_object_unref(pool);
 
-	return TRUE;
+  return TRUE;
 }
 
 int
 main(int argc, char *argv[]) {
-	GMainLoop *loop;
-	GstRTSPServer *server;
-	GstRTSPMediaMapping *mapping;
-	GstRTSPMediaFactory *factory_h264, *factory_jpg;
-	GstRTSPServerConfiguration * server_config = NULL;
+  GMainLoop *loop;
+  GstRTSPServer *server;
+  GstRTSPMediaMapping *mapping;
+  GstRTSPMediaFactory *factory_h264, *factory_jpg;
+  GstRTSPServerConfiguration * server_config = NULL;
 
-	gst_init(&argc, &argv);
+  gst_init(&argc, &argv);
 
-	loop = g_main_loop_new(NULL, FALSE);
+  loop = g_main_loop_new(NULL, FALSE);
 
-	/* create a server instance */
-	server = gst_rtsp_server_new();
+  /* create a server instance */
+  server = gst_rtsp_server_new();
 
-	/* get the mapping for this server, every server has a default mapper object
-	 * that be used to map uri mount points to media factories */
+  /* get the mapping for this server, every server has a default mapper object
+   * that be used to map uri mount points to media factories */
 
-	mapping = gst_rtsp_server_get_media_mapping(server);
+  mapping = gst_rtsp_server_get_media_mapping(server);
 
-	/* make a media factory for h264 video stream and audio(aac, g711 or g726) stream. The default media factory can use
-	 * gst-launch syntax to create pipelines.
-	 * any launch line works as long as it contains elements named pay%d. Each
-	 * element with pay%d names will be a stream */
-	factory_h264 = gst_rtsp_media_factory_new();
-	/* set webcam source and port to listen for server */
-	gst_rtsp_factory_set_device_source(factory_h264, "v4l2src", "/dev/video1", 3000);
+  /* make a media factory for h264 video stream and audio(aac, g711 or g726) stream. The default media factory can use
+   * gst-launch syntax to create pipelines.
+   * any launch line works as long as it contains elements named pay%d. Each
+   * element with pay%d names will be a stream */
+  factory_h264 = gst_rtsp_media_factory_new();
+  /* set webcam source and port to listen for server */
+  gst_rtsp_factory_set_device_source(factory_h264, "v4l2src", "/dev/video1", 3000);
 
-	/* prepare server configuration for h264 stream */
-	server_config = gst_rtsp_server_configuration_load(DEFAULT_PROFILE_FILE_VIDEO_H264);
+  /* prepare server configuration for h264 stream */
+  server_config = gst_rtsp_server_configuration_load(DEFAULT_PROFILE_FILE_VIDEO_H264);
 
-	/* check and set audio profile for h264 stream */
-	if (argc > 1 && server_config != NULL) {
-		gchar * audio_profile_name = NULL;
-		if (g_strrstr(argv[1], "aac")) {
-			audio_profile_name = "audio AAC";
-		} else if (g_strrstr(argv[1], "g726")) {
-			audio_profile_name = "audio G726";
-		} else if (g_strrstr(argv[1], "g711")) {
-			audio_profile_name = "audio G711";
-		}
-		if (audio_profile_name != NULL) {
-			gst_rtsp_server_configuration_set_default_audio_pipeline(server_config, audio_profile_name);
-		}
-	}
+  /* check and set audio profile for h264 stream */
+  if (argc > 1 && server_config != NULL) {
+    gchar * audio_profile_name = NULL;
+    if (g_strrstr(argv[1], "aac")) {
+      audio_profile_name = "audio AAC";
+    } else if (g_strrstr(argv[1], "g726")) {
+      audio_profile_name = "audio G726";
+    } else if (g_strrstr(argv[1], "g711")) {
+      audio_profile_name = "audio G711";
+    }
+    if (audio_profile_name != NULL) {
+      gst_rtsp_server_configuration_set_default_audio_pipeline(server_config, audio_profile_name);
+    }
+  }
 
-	/* map server configuration to h264 media factory */
-	gst_rtsp_media_factory_set_server_configuration(factory_h264, server_config);
+  /* map server configuration to h264 media factory */
+  gst_rtsp_media_factory_set_server_configuration(factory_h264, server_config);
 
-	/* share the pipeline with multiple clients */
-	gst_rtsp_media_factory_set_shared(factory_h264, TRUE);
+  /* share the pipeline with multiple clients */
+  gst_rtsp_media_factory_set_shared(factory_h264, TRUE);
 
-	/* attach the test factory to the /h264 url */
-	gst_rtsp_media_mapping_add_factory(mapping, "/h264", factory_h264);
+  /* attach the test factory to the /h264 url */
+  gst_rtsp_media_mapping_add_factory(mapping, "/h264", factory_h264);
 
-	/* make a media factory for jpeg stream. The default media factory can use
-	 * gst-launch syntax to create pipelines.
-	 * any launch line works as long as it contains elements named pay%d. Each
-	 * element with pay%d names will be a stream */
-	factory_jpg = gst_rtsp_media_factory_new();
-	/* set webcam source and port to listen for server */
-	gst_rtsp_factory_set_device_source(factory_jpg, "v4l2src", "/dev/video0", 4000);
+  /* make a media factory for jpeg stream. The default media factory can use
+   * gst-launch syntax to create pipelines.
+   * any launch line works as long as it contains elements named pay%d. Each
+   * element with pay%d names will be a stream */
+  factory_jpg = gst_rtsp_media_factory_new();
+  /* set webcam source and port to listen for server */
+  gst_rtsp_factory_set_device_source(factory_jpg, "v4l2src", "/dev/video0", 4000);
 
-	/* prepare server configuration for jpeg stream */
-	server_config = gst_rtsp_server_configuration_load(DEFAULT_PROFILE_FILE_JPEG);
-	/* map server configuration to jpeg media factory */
-	gst_rtsp_media_factory_set_server_configuration(factory_jpg, server_config);
+  /* prepare server configuration for jpeg stream */
+  server_config = gst_rtsp_server_configuration_load(DEFAULT_PROFILE_FILE_JPEG);
+  /* map server configuration to jpeg media factory */
+  gst_rtsp_media_factory_set_server_configuration(factory_jpg, server_config);
 
-	/* share the pipeline with multiple clients */
-	gst_rtsp_media_factory_set_shared(factory_jpg, TRUE);
+  /* share the pipeline with multiple clients */
+  gst_rtsp_media_factory_set_shared(factory_jpg, TRUE);
 
-	/* attach the test factory to the /jpg url */
-	gst_rtsp_media_mapping_add_factory(mapping, "/jpg", factory_jpg);
+  /* attach the test factory to the /jpg url */
+  gst_rtsp_media_mapping_add_factory(mapping, "/jpg", factory_jpg);
 
-	/* don't need the ref to the mapper anymore */
-	g_object_unref(mapping);
+  /* don't need the ref to the mapper anymore */
+  g_object_unref(mapping);
 
-	/* attach the server to the default maincontext */
-	gst_rtsp_server_attach(server, NULL);
+  /* attach the server to the default maincontext */
+  gst_rtsp_server_attach(server, NULL);
 
-	g_timeout_add_seconds(15, (GSourceFunc) timeout, server);
+  g_timeout_add_seconds(15, (GSourceFunc) timeout, server);
 
-	/* start serving */
-	g_main_loop_run(loop);
+  /* start serving */
+  g_main_loop_run(loop);
 
-	return 0;
+  return 0;
 }
