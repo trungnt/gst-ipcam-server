85a070d82ac831860199c65e9c2f5ccf2f9e3215
Dam Quang Tuan <damquan.tuan@nomovok.com>
2009-11-12 15:56:03 -0500

    changed max_latency to 2000 ms

--- client/gst-ipcam-client-backend.c
+++ client/gst-ipcam-client-backend.c
@@ -21,7 +21,7 @@
 #include "gst-ipcam-client-callbacks.h"
 
 /* amount of ms to buffer in the client */
-#define MAX_LATENCY 2000
+#define MAX_LATENCY 1000
 
 static gpointer window;
 static gchar *video_type;
